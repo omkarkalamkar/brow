9c541e084c6d1aeac8dc6b032bdd6c4d07099312
manthan <manthan.ska@gmail.com>
2025-01-13 17:43:17 +0530

    SAH-1638: test test case

--- tests/tmc/test_xtp_32140_low_delay_model.py
+++ tests/tmc/test_xtp_32140_low_delay_model.py
@@ -33,6 +33,7 @@ from tests.resources.test_harness.subarray_node_low import (
 from tests.resources.test_harness.utils.common_utils import JsonFactory
 
 
+@pytest.mark.test1
 @pytest.mark.SKA_low
 @scenario(
     "../features/tmc/xtp_32140_low_delay_model.feature",
