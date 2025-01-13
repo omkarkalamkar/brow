85482b3f0942638aae6feec9eb09f7af5a44f1e3
manthan <manthan.ska@gmail.com>
2025-01-13 16:41:41 +0530

    SAH-1638: Resolve lint issue

--- tests/tmc/test_xtp_68914_tmc_stops_generating_delay_model.py
+++ tests/tmc/test_xtp_68914_tmc_stops_generating_delay_model.py
@@ -33,6 +33,7 @@ from tests.resources.test_harness.subarray_node_low import (
 )
 from tests.resources.test_harness.utils.common_utils import JsonFactory
 
+
 @pytest.mark.test1
 @pytest.mark.SKA_low
 @scenario(
