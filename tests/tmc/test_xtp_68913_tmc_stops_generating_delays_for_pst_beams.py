56eacdb14c50d954f612adb2664fbfc258125143
manthan <manthan.ska@gmail.com>
2025-01-13 17:17:53 +0530

    SAH-1638: Resolve lint issue

--- tests/tmc/test_xtp_68913_tmc_stops_generating_delays_for_pst_beams.py
+++ tests/tmc/test_xtp_68913_tmc_stops_generating_delays_for_pst_beams.py
@@ -31,6 +31,7 @@ from tests.resources.test_harness.subarray_node_low import (
 )
 from tests.resources.test_harness.utils.common_utils import JsonFactory
 
+
 @pytest.mark.test1
 @pytest.mark.SKA_low
 @scenario(
