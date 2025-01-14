c71f3dab3a126e18c5abe286f2f16d96bb51ca30
SKAIN <truptin.ska@gmail.com>
2025-01-14 12:37:12 +0530

    SAH-1647:execute error test

--- tests/features/tmc/check_error_propagation.feature
+++ tests/features/tmc/check_error_propagation.feature
@@ -14,3 +14,20 @@ Examples:
             |SCANNING         | ENDSCAN | MCCS                 | SCANNING |
             |READY            | SCAN    | CSP                  | SCANNING |
             |READY            | SCAN    | MCCS                 | SCANNING |
+
+
+Scenario: TimeOut Reported by TMC Low End/EndScan/Scan Commands for Defective Subarray
+
+Given the telescope is is ON state
+And the TMC subarray is in the <initialObsState> observation state
+When <command> is invoked on a <defectiveSubsystem> Subarray
+Then the command failure is reported by subarray with appropriate error message
+Then the TMC SubarrayNode remains in <Intermediate> obsState
+Examples:
+            |initialObsState  | command | defectiveSubsystem  |Intermediate|
+            |READY            | END     | CSP                  | READY |
+            |READY            | END     | MCCS                 | READY |
+            |SCANNING         | ENDSCAN | CSP                  | SCANNING |
+            |SCANNING         | ENDSCAN | MCCS                 | SCANNING |
+            |READY            | SCAN    | CSP                  | SCANNING |
+            |READY            | SCAN    | MCCS                 | SCANNING |
--- tests/tmc/test_error_propogation_xtp_73766.py
+++ tests/tmc/test_error_propogation_xtp_73766.py
@@ -8,10 +8,33 @@ is identified as defective,
  then Subarry node
    reports an error.
 """
-
+import json
 
 import pytest
-from pytest_bdd import scenario
+from assertpy import assert_that
+from pytest_bdd import parsers, scenario, then, when
+from ska_control_model import ResultCode
+from ska_tango_testing.integration import TangoEventTracer
+
+from tests.conftest import LOGGER
+from tests.resources.test_harness.constant import (
+    INTERMEDIATE_CONFIGURING_STATE_DEFECT,
+    TIMEOUT,
+    low_csp_subarray_leaf_node,
+    low_sdp_subarray_leaf_node,
+    mccs_subarray_leaf_node,
+)
+from tests.resources.test_harness.simulator_factory import SimulatorFactory
+from tests.resources.test_harness.subarray_node_low import (
+    SubarrayNodeWrapperLow,
+)
+from tests.resources.test_harness.utils.common_utils import JsonFactory
+from tests.resources.test_harness.utils.enums import SimulatorDeviceType
+from tests.tmc.conftest import (
+    perform_ready_transition_with_end,
+    perform_scan,
+    verify_scanning_transition_with_endscan,
+)
 
 
 @pytest.mark.SKA_low12
@@ -24,3 +47,127 @@ def test_tmc_command_error_propagation():
     """
     Test case to verify TMC Error Propagation functionality.
     """
+
+
+@pytest.mark.SKA_low13
+@scenario(
+    "../features/tmc/check_error_propagation.feature",
+    "TimeOut Reported by TMC Low End/EndScan/Scan "
+    "Commands for Defective Subarray",
+)
+def test_tmc_command_timeout_error_propagation():
+    """
+    Test case to verify TMC TimeOut Error Propagation functionality.
+    """
+
+
+@when(parsers.parse("{command} is invoked on a {defectiveSubsystem} Subarray"))
+def execute_command_on_tmc_with_defectivesetup(
+    subarray_node_low: SubarrayNodeWrapperLow,
+    event_tracer: TangoEventTracer,
+    simulator_factory: SimulatorFactory,
+    command_input_factory: JsonFactory,
+    command,
+    defectiveSubsystem,
+):
+    """
+    Send next command on TMC
+    """
+
+    LOGGER.info("Inside %s  is invoked for %s", command, defectiveSubsystem)
+
+    pytest.defective_subarray = None
+    match defectiveSubsystem:
+        case "CSP":
+            pytest.defective_subarray = (
+                simulator_factory.get_or_create_simulator_device(
+                    SimulatorDeviceType.LOW_CSP_DEVICE
+                )
+            )
+            pytest.defective_device = low_csp_subarray_leaf_node
+        case "SDP":
+            pytest.defective_subarray = (
+                simulator_factory.get_or_create_simulator_device(
+                    SimulatorDeviceType.LOW_SDP_DEVICE
+                )
+            )
+            pytest.defective_device = low_sdp_subarray_leaf_node
+
+        case "MCCS":
+            pytest.defective_subarray = (
+                simulator_factory.get_or_create_simulator_device(
+                    SimulatorDeviceType.MCCS_SUBARRAY_DEVICE
+                )
+            )
+
+            pytest.defective_device = mccs_subarray_leaf_node
+
+    pytest.defective_subarray.SetDefective(
+        INTERMEDIATE_CONFIGURING_STATE_DEFECT
+    )
+
+    match command:
+        case "END":
+            LOGGER.info("Working on Ready State")
+            perform_ready_transition_with_end(
+                subarray_node_low,
+                event_tracer,
+            )
+
+        case "ENDSCAN":
+            LOGGER.info("Working on End Scan")
+            verify_scanning_transition_with_endscan(
+                subarray_node_low,
+                # event_tracer,
+            )
+        case "SCAN":
+            LOGGER.info("Workng on Scan")
+            perform_scan(
+                subarray_node_low,
+                # event_tracer,
+                command_input_factory,
+            )
+
+
+@then(
+    parsers.parse(
+        "the command failure is reported by subarray with appropriate"
+        " error message"
+    )
+)
+def validate_error_message_reporting(
+    subarray_node_low: SubarrayNodeWrapperLow,
+    event_tracer: TangoEventTracer,
+):
+    """
+    Send next command on TMC
+    """
+
+    LOGGER.info("validate_error_message_reporting")
+
+    # exception_message = (
+    #     "Exception occurred on the following devices:"
+    #     + f" {pytest.defective_device}:"
+    #     + " Exception occurred, command failed."
+    # )
+
+    exception_message = "Timeout has occurred, command failed"
+
+    assert_that(event_tracer).described_as(
+        'FAILED ASSUMPTION IN "THEN" STEP: '
+        '"the command failure is reported by subarray with appropriate"'
+        '"error message"'
+        "Subarray Node device"
+        f"({subarray_node_low.subarray_node.dev_name()}) "
+        "is expected have longRunningCommandResult"
+        "(ResultCode.FAILED,exception)",
+    ).within_timeout(TIMEOUT).has_desired_result_code_message_in_lrcr_event(
+        subarray_node_low.subarray_node,
+        [exception_message],
+        pytest.unique_id[0],
+        ResultCode.FAILED,
+    )
+
+    pytest.defective_subarray.SetDefective(json.dumps({"enabled": False}))
+
+    event_tracer.clear_events()
