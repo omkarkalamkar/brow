09b182863e68dc8409233f78115ed2b64c0ff68b
manthan <manthan.ska@gmail.com>
2025-01-14 12:35:22 +0530

    SAH-1638: Revert change

--- tests/tmc_sdp/test_xtp_34886_sdp_json_error_duplicate_id.py
+++ tests/tmc_sdp/test_xtp_34886_sdp_json_error_duplicate_id.py
@@ -20,7 +20,6 @@ from tests.resources.test_harness.helpers import (
 )
 
 
-@pytest.mark.test1
 @pytest.mark.tmc_sdp
 @scenario(
     "../features/tmc_sdp/xtp_34886_sdp_json_error_duplicate_id.feature",
