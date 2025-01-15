9a8e3acbb51c3b4c35a8637bf84dd8230e0f0966
manthan <manthan.ska@gmail.com>
2025-01-15 17:36:57 +0530

    SAH-1638: Update xtp

--- tests/tmc_mccs/test_xtp-31009_mccs_configure_command.py
+++ tests/tmc_mccs/test_xtp-31009_mccs_configure_command.py
@@ -16,6 +16,7 @@ from tests.resources.test_harness.helpers import (
 from tests.resources.test_support.common_utils.result_code import ResultCode
 
 
+@pytest.mark.test1
 @pytest.mark.tmc_mccs
 @scenario(
     "../features/tmc_mccs/xtp-31009_configure.feature",
