9b3ddbd6250117c10c16eaae7a74817329cf6c0b
truptin.ska <truptin.ska@gmail.com>
2025-02-07 19:33:32 +0530

    SAH-1662:low test

--- tests/tmc/test_error_propogation_xtp_73766.py
+++ tests/tmc/test_error_propogation_xtp_73766.py
@@ -37,7 +37,7 @@ from tests.tmc.conftest import (
 )
 
 
-@pytest.mark.SKA_low
+@pytest.mark.SKA_low25
 @scenario(
     "../features/tmc/check_error_propagation.feature",
     "Error Propagation Reported by TMC Low End/EndScan/Scan "
