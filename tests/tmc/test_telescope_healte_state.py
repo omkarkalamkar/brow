5375de49ee2e79af79827b9f66dfee037370d3cc
Akshay Sonawane <akshayss.ska@gmail.com>
2025-05-05 13:34:53 +0530

    HM-663: update the test case to BDD

--- tests/tmc/test_telescope_healte_state.py
+++ tests/tmc/test_telescope_healte_state.py
@@ -1,5 +1,228 @@
 """Test Telescope Health State"""
+# import pytest
+# from ska_tango_base.control_model import HealthState
+# from tango import DevState
+
+# from tests.resources.test_harness.helpers import (
+#     get_device_simulators,
+#     get_master_device_simulators,
+# )
+
+
+# class TestTelescopeHealthState:
+#     """Test cases to verify telescopeHealthState of CentralNode.
+#     This implements rows of decision table for telescopeHealthState.
+#     """
+
+#     @pytest.mark.parametrize(
+#         "csp_master_health_state, sdp_master_health_state, "
+#         "mccs_master_health_state",
+#         [
+#             (HealthState.OK, HealthState.FAILED, HealthState.OK),
+#             (HealthState.FAILED, HealthState.OK, HealthState.OK),
+#             (HealthState.OK, HealthState.OK, HealthState.FAILED),
+#             (HealthState.OK, HealthState.FAILED, HealthState.FAILED),
+#             (HealthState.FAILED, HealthState.FAILED, HealthState.OK),
+#             (HealthState.FAILED, HealthState.OK, HealthState.FAILED),
+#             (HealthState.FAILED, HealthState.FAILED, HealthState.FAILED),
+#         ],
+#     )
+#     @pytest.mark.SKA_low
+#     def test_telescope_health_state_failed(
+#         self,
+#         central_node_low,
+#         simulator_factory,
+#         event_recorder,
+#         csp_master_health_state,
+#         sdp_master_health_state,
+#         mccs_master_health_state,
+#     ):
+#         """Test for healthstate FAILED"""
+#         (
+#             csp_master_sim,
+#             sdp_master_sim,
+#             mccs_master_sim,
+#         ) = get_master_device_simulators(simulator_factory)
+#         central_node_low.move_to_on()
+#         event_recorder.subscribe_event(
+#             central_node_low.central_node, "telescopeState"
+#         )
+#         assert event_recorder.has_change_event_occurred(
+#             central_node_low.central_node,
+#             "telescopeState",
+#             DevState.ON,
+#         )
+
+#         csp_master_sim.SetDirectHealthState(csp_master_health_state)
+#         sdp_master_sim.SetDirectHealthState(sdp_master_health_state)
+#         mccs_master_sim.SetDirectHealthState(mccs_master_health_state)
+
+#         event_recorder.subscribe_event(
+#             central_node_low.central_node, "telescopeHealthState"
+#         )
+
+#         assert event_recorder.has_change_event_occurred(
+#             central_node_low.central_node,
+#             "telescopeHealthState",
+#             HealthState.FAILED,
+#         )
+
+#     @pytest.mark.SKA_low
+#     def test_telescope_health_state_ok(
+#         self,
+#         central_node_low,
+#         subarray_node_low,
+#         simulator_factory,
+#         event_recorder,
+#     ):
+#         """Test for healthstate OK"""
+#         (
+#             csp_master_sim,
+#             sdp_master_sim,
+#             mccs_master_sim,
+#         ) = get_master_device_simulators(simulator_factory)
+#         (
+#             csp_subarray_sim,
+#             sdp_subarray_sim,
+#             mccs_subarray_sim,
+#         ) = get_device_simulators(simulator_factory)
+#         central_node_low.move_to_on()
+#         event_recorder.subscribe_event(
+#             central_node_low.central_node, "telescopeState"
+#         )
+#         assert event_recorder.has_change_event_occurred(
+#             central_node_low.central_node,
+#             "telescopeState",
+#             DevState.ON,
+#         )
+#         csp_master_sim.SetDirectHealthState(HealthState.OK)
+#         sdp_master_sim.SetDirectHealthState(HealthState.OK)
+#         mccs_master_sim.SetDirectHealthState(HealthState.OK)
+
+#         # Subarray healthstate should be OK
+#         csp_subarray_sim.SetDirectHealthState(HealthState.OK)
+#         sdp_subarray_sim.SetDirectHealthState(HealthState.OK)
+#         mccs_subarray_sim.SetDirectHealthState(HealthState.OK)
+#         event_recorder.subscribe_event(
+#             central_node_low.central_node, "telescopeHealthState"
+#         )
+#         event_recorder.subscribe_event(
+#             subarray_node_low.subarray_node, "healthState"
+#         )
+
+#         assert event_recorder.has_change_event_occurred(
+#             subarray_node_low.subarray_node,
+#             "healthState",
+#             HealthState.OK,
+#         )
+#         assert event_recorder.has_change_event_occurred(
+#             central_node_low.central_node,
+#             "telescopeHealthState",
+#             HealthState.OK,
+#         )
+
+#     @pytest.mark.parametrize(
+#         "csp_master_health_state, sdp_master_health_state, "
+#         "mccs_master_health_state",
+#         [
+#             (HealthState.OK, HealthState.DEGRADED, HealthState.OK),
+#             (HealthState.DEGRADED, HealthState.OK, HealthState.OK),
+#             (HealthState.OK, HealthState.OK, HealthState.DEGRADED),
+#             (HealthState.DEGRADED, HealthState.DEGRADED, HealthState.OK),
+#             (HealthState.OK, HealthState.DEGRADED, HealthState.DEGRADED),
+#         ],
+#     )
+#     @pytest.mark.SKA_low
+#     def test_telescope_health_state_degraded(
+#         self,
+#         central_node_low,
+#         simulator_factory,
+#         event_recorder,
+#         csp_master_health_state,
+#         sdp_master_health_state,
+#         mccs_master_health_state,
+#     ):
+#         """Test for healthstate DEGRADED"""
+#         (
+#             csp_master_sim,
+#             sdp_master_sim,
+#             mccs_master_sim,
+#         ) = get_master_device_simulators(simulator_factory)
+#         central_node_low.move_to_on()
+#         event_recorder.subscribe_event(
+#             central_node_low.central_node, "telescopeState"
+#         )
+#         assert event_recorder.has_change_event_occurred(
+#             central_node_low.central_node,
+#             "telescopeState",
+#             DevState.ON,
+#         )
+#         csp_master_sim.SetDirectHealthState(csp_master_health_state)
+#         sdp_master_sim.SetDirectHealthState(sdp_master_health_state)
+#         mccs_master_sim.SetDirectHealthState(mccs_master_health_state)
+
+#         event_recorder.subscribe_event(
+#             central_node_low.central_node, "telescopeHealthState"
+#         )
+
+#         assert event_recorder.has_change_event_occurred(
+#             central_node_low.central_node,
+#             "telescopeHealthState",
+#             HealthState.DEGRADED,
+#         )
+
+#     @pytest.mark.parametrize(
+#         "csp_master_health_state, sdp_master_health_state, "
+#         "mccs_master_health_state",
+#         [
+#             (HealthState.OK, HealthState.UNKNOWN, HealthState.OK),
+#             (HealthState.UNKNOWN, HealthState.OK, HealthState.OK),
+#             (HealthState.OK, HealthState.OK, HealthState.UNKNOWN),
+#             (HealthState.UNKNOWN, HealthState.UNKNOWN, HealthState.OK),
+#             (HealthState.UNKNOWN, HealthState.OK, HealthState.UNKNOWN),
+#         ],
+#     )
+#     @pytest.mark.SKA_low
+#     def test_telescope_health_state_unknown(
+#         self,
+#         central_node_low,
+#         simulator_factory,
+#         event_recorder,
+#         csp_master_health_state,
+#         sdp_master_health_state,
+#         mccs_master_health_state,
+#     ):
+#         """Test for healthstate UNKNOWN"""
+#         (
+#             csp_master_sim,
+#             sdp_master_sim,
+#             mccs_master_sim,
+#         ) = get_master_device_simulators(simulator_factory)
+#         central_node_low.move_to_on()
+#         event_recorder.subscribe_event(
+#             central_node_low.central_node, "telescopeState"
+#         )
+#         assert event_recorder.has_change_event_occurred(
+#             central_node_low.central_node,
+#             "telescopeState",
+#             DevState.ON,
+#         )
+#         csp_master_sim.SetDirectHealthState(csp_master_health_state)
+#         sdp_master_sim.SetDirectHealthState(sdp_master_health_state)
+#         mccs_master_sim.SetDirectHealthState(mccs_master_health_state)
+
+#         event_recorder.subscribe_event(
+#             central_node_low.central_node, "telescopeHealthState"
+#         )
+
+#         assert event_recorder.has_change_event_occurred(
+#             central_node_low.central_node,
+#             "telescopeHealthState",
+#             HealthState.UNKNOWN,
+#         )
+
 import pytest
+from pytest_bdd import given, parsers, scenarios, then, when
 from ska_tango_base.control_model import HealthState
 from tango import DevState
 
@@ -8,215 +231,88 @@ from tests.resources.test_harness.helpers import (
     get_master_device_simulators,
 )
 
+state = {}
 
-class TestTelescopeHealthState:
-    """Test cases to verify telescopeHealthState of CentralNode.
-    This implements rows of decision table for telescopeHealthState.
-    """
+scenarios("../features/tmc/check_telescope_healthstate.feature")
 
-    @pytest.mark.parametrize(
-        "csp_master_health_state, sdp_master_health_state, "
-        "mccs_master_health_state",
-        [
-            (HealthState.OK, HealthState.FAILED, HealthState.OK),
-            (HealthState.FAILED, HealthState.OK, HealthState.OK),
-            (HealthState.OK, HealthState.OK, HealthState.FAILED),
-            (HealthState.OK, HealthState.FAILED, HealthState.FAILED),
-            (HealthState.FAILED, HealthState.FAILED, HealthState.OK),
-            (HealthState.FAILED, HealthState.OK, HealthState.FAILED),
-            (HealthState.FAILED, HealthState.FAILED, HealthState.FAILED),
-        ],
+
+@pytest.mark.aki
+def test_telescope_health_state():
+    """Test telescope healthstate"""
+
+
+@given("the telescope is ON")
+def telescope_on(central_node_low, event_recorder):
+    """Turn On the telescope"""
+    state["central_node"] = central_node_low.central_node
+    central_node_low.move_to_on()
+    event_recorder.subscribe_event(state["central_node"], "telescopeState")
+    assert event_recorder.has_change_event_occurred(
+        state["central_node"], "telescopeState", DevState.ON
     )
-    @pytest.mark.SKA_low
-    def test_telescope_health_state_failed(
-        self,
-        central_node_low,
-        simulator_factory,
-        event_recorder,
-        csp_master_health_state,
-        sdp_master_health_state,
-        mccs_master_health_state,
-    ):
-        """Test for healthstate FAILED"""
-        (
-            csp_master_sim,
-            sdp_master_sim,
-            mccs_master_sim,
-        ) = get_master_device_simulators(simulator_factory)
-        central_node_low.move_to_on()
-        event_recorder.subscribe_event(
-            central_node_low.central_node, "telescopeState"
-        )
-        assert event_recorder.has_change_event_occurred(
-            central_node_low.central_node,
-            "telescopeState",
-            DevState.ON,
-        )
 
-        csp_master_sim.SetDirectHealthState(csp_master_health_state)
-        sdp_master_sim.SetDirectHealthState(sdp_master_health_state)
-        mccs_master_sim.SetDirectHealthState(mccs_master_health_state)
 
-        event_recorder.subscribe_event(
-            central_node_low.central_node, "telescopeHealthState"
-        )
-
-        assert event_recorder.has_change_event_occurred(
-            central_node_low.central_node,
-            "telescopeHealthState",
-            HealthState.FAILED,
-        )
-
-    @pytest.mark.SKA_low
-    def test_telescope_health_state_ok(
-        self,
-        central_node_low,
-        subarray_node_low,
-        simulator_factory,
-        event_recorder,
-    ):
-        """Test for healthstate OK"""
-        (
-            csp_master_sim,
-            sdp_master_sim,
-            mccs_master_sim,
-        ) = get_master_device_simulators(simulator_factory)
-        (
-            csp_subarray_sim,
-            sdp_subarray_sim,
-            mccs_subarray_sim,
-        ) = get_device_simulators(simulator_factory)
-        central_node_low.move_to_on()
-        event_recorder.subscribe_event(
-            central_node_low.central_node, "telescopeState"
-        )
-        assert event_recorder.has_change_event_occurred(
-            central_node_low.central_node,
-            "telescopeState",
-            DevState.ON,
-        )
-        csp_master_sim.SetDirectHealthState(HealthState.OK)
-        sdp_master_sim.SetDirectHealthState(HealthState.OK)
-        mccs_master_sim.SetDirectHealthState(HealthState.OK)
-
-        # Subarray healthstate should be OK
-        csp_subarray_sim.SetDirectHealthState(HealthState.OK)
-        sdp_subarray_sim.SetDirectHealthState(HealthState.OK)
-        mccs_subarray_sim.SetDirectHealthState(HealthState.OK)
-        event_recorder.subscribe_event(
-            central_node_low.central_node, "telescopeHealthState"
-        )
-        event_recorder.subscribe_event(
-            subarray_node_low.subarray_node, "healthState"
-        )
-
-        assert event_recorder.has_change_event_occurred(
-            subarray_node_low.subarray_node,
-            "healthState",
-            HealthState.OK,
-        )
-        assert event_recorder.has_change_event_occurred(
-            central_node_low.central_node,
-            "telescopeHealthState",
-            HealthState.OK,
-        )
-
-    @pytest.mark.parametrize(
-        "csp_master_health_state, sdp_master_health_state, "
-        "mccs_master_health_state",
-        [
-            (HealthState.OK, HealthState.DEGRADED, HealthState.OK),
-            (HealthState.DEGRADED, HealthState.OK, HealthState.OK),
-            (HealthState.OK, HealthState.OK, HealthState.DEGRADED),
-            (HealthState.DEGRADED, HealthState.DEGRADED, HealthState.OK),
-            (HealthState.OK, HealthState.DEGRADED, HealthState.DEGRADED),
-        ],
+@given(parsers.parse("CSP master health is {csp_state}"))
+@given(parsers.parse("SDP master health is {sdp_state}"))
+@given(parsers.parse("MCCS master health is {mccs_state}"))
+def set_master_health(
+    simulator_factory, csp_state=None, sdp_state=None, mccs_state=None
+):
+    """Set master devices healthstate"""
+    csp, sdp, mccs = get_master_device_simulators(simulator_factory)
+    state["csp"], state["sdp"], state["mccs"] = csp, sdp, mccs
+    state["csp_state"], state["sdp_state"], state["mccs_state"] = (
+        csp_state,
+        sdp_state,
+        mccs_state,
     )
-    @pytest.mark.SKA_low
-    def test_telescope_health_state_degraded(
-        self,
-        central_node_low,
-        simulator_factory,
-        event_recorder,
-        csp_master_health_state,
-        sdp_master_health_state,
-        mccs_master_health_state,
-    ):
-        """Test for healthstate DEGRADED"""
-        (
-            csp_master_sim,
-            sdp_master_sim,
-            mccs_master_sim,
-        ) = get_master_device_simulators(simulator_factory)
-        central_node_low.move_to_on()
-        event_recorder.subscribe_event(
-            central_node_low.central_node, "telescopeState"
-        )
-        assert event_recorder.has_change_event_occurred(
-            central_node_low.central_node,
-            "telescopeState",
-            DevState.ON,
-        )
-        csp_master_sim.SetDirectHealthState(csp_master_health_state)
-        sdp_master_sim.SetDirectHealthState(sdp_master_health_state)
-        mccs_master_sim.SetDirectHealthState(mccs_master_health_state)
 
-        event_recorder.subscribe_event(
-            central_node_low.central_node, "telescopeHealthState"
-        )
 
-        assert event_recorder.has_change_event_occurred(
-            central_node_low.central_node,
-            "telescopeHealthState",
-            HealthState.DEGRADED,
-        )
-
-    @pytest.mark.parametrize(
-        "csp_master_health_state, sdp_master_health_state, "
-        "mccs_master_health_state",
-        [
-            (HealthState.OK, HealthState.UNKNOWN, HealthState.OK),
-            (HealthState.UNKNOWN, HealthState.OK, HealthState.OK),
-            (HealthState.OK, HealthState.OK, HealthState.UNKNOWN),
-            (HealthState.UNKNOWN, HealthState.UNKNOWN, HealthState.OK),
-            (HealthState.UNKNOWN, HealthState.OK, HealthState.UNKNOWN),
-        ],
+@given("all master and subarray components have OK health")
+def set_all_ok_health(simulator_factory):
+    """Set all healthstate to OK"""
+    csp_m, sdp_m, mccs_m = get_master_device_simulators(simulator_factory)
+    csp_s, sdp_s, mccs_s = get_device_simulators(simulator_factory)
+    for device in [csp_m, sdp_m, mccs_m, csp_s, sdp_s, mccs_s]:
+        device.SetDirectHealthState(HealthState.OK)
+    state.update(
+        {
+            "csp": csp_m,
+            "sdp": sdp_m,
+            "mccs": mccs_m,
+            "subarray_devices": (csp_s, sdp_s, mccs_s),
+        }
     )
-    @pytest.mark.SKA_low
-    def test_telescope_health_state_unknown(
-        self,
-        central_node_low,
-        simulator_factory,
-        event_recorder,
-        csp_master_health_state,
-        sdp_master_health_state,
-        mccs_master_health_state,
-    ):
-        """Test for healthstate UNKNOWN"""
-        (
-            csp_master_sim,
-            sdp_master_sim,
-            mccs_master_sim,
-        ) = get_master_device_simulators(simulator_factory)
-        central_node_low.move_to_on()
-        event_recorder.subscribe_event(
-            central_node_low.central_node, "telescopeState"
-        )
-        assert event_recorder.has_change_event_occurred(
-            central_node_low.central_node,
-            "telescopeState",
-            DevState.ON,
-        )
-        csp_master_sim.SetDirectHealthState(csp_master_health_state)
-        sdp_master_sim.SetDirectHealthState(sdp_master_health_state)
-        mccs_master_sim.SetDirectHealthState(mccs_master_health_state)
 
-        event_recorder.subscribe_event(
-            central_node_low.central_node, "telescopeHealthState"
-        )
 
-        assert event_recorder.has_change_event_occurred(
-            central_node_low.central_node,
-            "telescopeHealthState",
-            HealthState.UNKNOWN,
-        )
+@when("health states are applied")
+def apply_health_states():
+    """Apply the healthstate to devices"""
+    for name in ["csp", "sdp", "mccs"]:
+        device = state[name]
+        raw_state = state.get(f"{name}_state", "OK")
+        device.SetDirectHealthState(HealthState[raw_state])
+
+
+@then(parsers.parse("the telescopeHealthState should be {expected_state}"))
+def check_telescope_health_state(event_recorder, expected_state):
+    """Verify the telescope healthstate"""
+    event_recorder.subscribe_event(
+        state["central_node"], "telescopeHealthState"
+    )
+    assert event_recorder.has_change_event_occurred(
+        state["central_node"],
+        "telescopeHealthState",
+        HealthState[expected_state],
+    )
+
+
+@then("the subarray healthState should be OK")
+def check_subarray_health(event_recorder, subarray_node_low):
+    """Verify the subarray healthstate"""
+    event_recorder.subscribe_event(
+        subarray_node_low.subarray_node, "healthState"
+    )
+    assert event_recorder.has_change_event_occurred(
+        subarray_node_low.subarray_node, "healthState", HealthState.OK
+    )
