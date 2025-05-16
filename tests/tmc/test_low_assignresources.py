eae2eef3088a52c43149adc6230352b0a170a141
ganesh.shinde <ganeshshinde.ska@gmail.com>
2025-05-16 11:59:17 +0530

    SAH-1722: testing assignresources testcase

--- tests/tmc/test_low_assignresources.py
+++ tests/tmc/test_low_assignresources.py
@@ -11,6 +11,7 @@ from ska_integration_test_harness.facades.sdp_facade import SDPFacade
 from ska_integration_test_harness.facades.tmc_facade import TMCFacade
 from ska_tango_testing.integration import TangoEventTracer
 
+from tests.conftest import LOGGER
 from tests.resources.test_harness.constant import COMMAND_COMPLETED
 from tests.resources.test_harness.helpers import check_subarray_obsstate
 from tests.resources.test_harness.utils.my_file_json_input import (
@@ -94,6 +95,7 @@ def subsystems_subarray_idle(
 ):
     """Check if all subarrays are in IDLE obsState."""
     check_subarray_obsstate(tmc, csp, sdp, mccs, event_tracer, ObsState.IDLE)
+    LOGGER.info("AR value: %s", tmc.subarray_node.assignedResources)
     assigned_resources_json = MyFileJSONInput(
         "centralnode", "assign_resources_low"
     ).as_str()
