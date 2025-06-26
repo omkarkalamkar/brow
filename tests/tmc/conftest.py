c444680c3d9d5cbfefde3112510b1993e1c19399
ganesh.shinde <ganeshshinde.ska@gmail.com>
2025-06-26 17:14:26 +0530

    SAH-1737: minor fix

--- tests/tmc/conftest.py
+++ tests/tmc/conftest.py
@@ -559,8 +559,6 @@ def validate_error_message_reporting(
 
     pytest.defective_subarray.SetDefective(json.dumps({"enabled": False}))
 
-    event_tracer.clear_events()
-
 
 @then(parsers.parse("the TMC SubarrayNode transitions to FAULT obsState"))
 def validate_subarry_obsState(
