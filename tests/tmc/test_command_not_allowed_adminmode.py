3ad1290798974f9604ecc3c77dbeb15347b72bfe
Akshay Sonawane <akshayss.ska@gmail.com>
2025-06-26 19:32:36 +0530

    HM-699: update test case

--- tests/tmc/test_command_not_allowed_adminmode.py
+++ tests/tmc/test_command_not_allowed_adminmode.py
@@ -25,7 +25,7 @@ SUBSYSTEM_DEVICES = {
 @pytest.mark.aki
 @scenario(
     "../features/tmc/check_cmd_not_allowed_adminmode.feature",
-    "Command not allowed when subsystem adminmode is OFFLINE/NOT_FITTED",
+    "Command not allowed from CentralNode when subsystem adminmode is OFFLINE/NOT_FITTED",
 )
 def test_command_not_allowed():
     """
