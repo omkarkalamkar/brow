3df9ee9cf54358c6f83432333560992cc89d1de5
apurvapatkar <apurva.ska@gmail.com>
2025-06-30 13:05:37 +0530

    SAH-1736: Execute tests.

--- tests/tmc/test_recover_subarray_stuck_in_resourcing_low.py
+++ tests/tmc/test_recover_subarray_stuck_in_resourcing_low.py
@@ -191,7 +191,7 @@ def test_recover_subarray_stuck_in_resourcing_low(
 
 
 @pytest.mark.SKA_low
-@pytest.mark.skip(reason="Restart timeout error")
+# @pytest.mark.skip(reason="Restart timeout error")
 @pytest.mark.parametrize("defective_device", ["csp_subarray", "sdp_subarray"])
 def test_restart_with_sdp_csp_in_empty(
     central_node_low: CentralNodeWrapperLow,
@@ -383,7 +383,7 @@ def test_restart_with_sdp_csp_in_empty(
 
 
 @pytest.mark.SKA_low
-@pytest.mark.skip(reason="Restart timeout error")
+# @pytest.mark.skip(reason="Restart timeout error")
 def test_abort_with_mccs_in_empty(
     subarray_node_low: SubarrayNodeWrapperLow,
     central_node_low: CentralNodeWrapperLow,
