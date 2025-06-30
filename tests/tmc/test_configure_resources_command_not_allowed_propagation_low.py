ad766c9bdcf6c2b42d36a9afa50386b7daf4135f
truptin.ska <truptin.ska@gmail.com>
2025-06-30 14:10:26 +0530

    sah-1735:Execute more configure

--- tests/tmc/test_configure_resources_command_not_allowed_propagation_low.py
+++ tests/tmc/test_configure_resources_command_not_allowed_propagation_low.py
@@ -32,7 +32,7 @@ class TestConfigureCommandNotAllowedPropagation:
     """Test the command not allowed error propagation for the assign resources
     command for TMC."""
 
-    @pytest.mark.SKA_low15
+    @pytest.mark.SKA_low
     def test_configure_command_not_allowed_propagation_csp_ln_low(
         self,
         central_node_low: CentralNodeWrapperLow,
