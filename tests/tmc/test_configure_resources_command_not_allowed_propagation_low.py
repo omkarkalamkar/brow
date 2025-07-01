1aff367fb4807c5eee8c140436b675118c377b99
truptin.ska <truptin.ska@gmail.com>
2025-07-01 07:11:27 +0530

    sah-1735:Execute configure tests

--- tests/tmc/test_configure_resources_command_not_allowed_propagation_low.py
+++ tests/tmc/test_configure_resources_command_not_allowed_propagation_low.py
@@ -11,10 +11,10 @@ from tests.resources.test_harness.central_node_low import CentralNodeWrapperLow
 from tests.resources.test_harness.constant import (
     COMMAND_NOT_ALLOWED_DEFECT,
     TIMEOUT,
-    low_sdp_subarray_leaf_node,
 )
 
 # mccs_controller,; mccs_master_leaf_node,
+# low_sdp_subarray_leaf_node,
 from tests.resources.test_harness.simulator_factory import SimulatorFactory
 from tests.resources.test_harness.subarray_node_low import (
     SubarrayNodeWrapperLow,
@@ -58,11 +58,9 @@ class TestConfigureCommandNotAllowedPropagation:
         event_tracer.subscribe_event(
             subarray_node_low.subarray_node, "longRunningCommandResult"
         )
-
-        # Preparing input arguments
-        # assign_input_json = prepare_json_args_for_centralnode_commands(
-        #     "assign_resources_low", command_input_factory
-        # )
+        event_tracer.subscribe_event(
+            subarray_node_low.subarray_node, "obsState"
+        )
 
         central_node_low.move_to_on()
         assert_that(event_tracer).described_as(
@@ -76,9 +74,6 @@ class TestConfigureCommandNotAllowedPropagation:
             DevState.ON,
         )
 
-        event_tracer.subscribe_event(
-            subarray_node_low.subarray_node, "obsState"
-        )
         assign_input_str = prepare_json_args_for_centralnode_commands(
             "assign_resources_low", command_input_factory
         )
@@ -124,13 +119,9 @@ class TestConfigureCommandNotAllowedPropagation:
             + "Subarray Device low-csp/subarray/01"
         )
 
-        exception_message = (
-            "ska_tmc_common.exceptions.CommandNotAllowed: "
-            "Command is not allowed"
+        log_events(
+            {subarray_node_low.subarray_node: ["longRunningCommandResult"]}
         )
-        # log_events(
-        #     {subarray_node_low.subarray_node: ["longRunningCommandResult"]}
-        # )
 
         assert_that(event_tracer).described_as(
             "FAILED ASSUMPTION AFTER CONFIGURE: "
@@ -146,15 +137,16 @@ class TestConfigureCommandNotAllowedPropagation:
             ResultCode.FAILED,
         )
 
-    @pytest.mark.SKA_low
-    def test_assign_command_not_allowed_propagation_sdp_ln_low(
+    @pytest.mark.SKA_low16
+    def test_configure_command_not_allowed_propagation_sdp_ln_low(
         self,
         central_node_low: CentralNodeWrapperLow,
+        subarray_node_low: SubarrayNodeWrapperLow,
         event_tracer: TangoEventTracer,
         simulator_factory: SimulatorFactory,
         command_input_factory: JsonFactory,
     ):
-        """Verify command not allowed exception propagation from SDPLeafNodes
+        """Verify command not allowed exception propagation from CSPLeafNodes
         ."""
         sdp_subarray_sim = simulator_factory.get_or_create_simulator_device(
             SimulatorDeviceType.LOW_SDP_DEVICE
@@ -165,15 +157,14 @@ class TestConfigureCommandNotAllowedPropagation:
             central_node_low.central_node, "telescopeState"
         )
         event_tracer.subscribe_event(
-            central_node_low.subarray_node, "obsState"
-        )
-        event_tracer.subscribe_event(
             central_node_low.central_node, "longRunningCommandResult"
         )
-        event_tracer.subscribe_event(sdp_subarray_sim, "obsState")
-        # Preparing input arguments
-        assign_input_json = prepare_json_args_for_centralnode_commands(
-            "assign_resources_low", command_input_factory
+
+        event_tracer.subscribe_event(
+            subarray_node_low.subarray_node, "longRunningCommandResult"
+        )
+        event_tracer.subscribe_event(
+            subarray_node_low.subarray_node, "obsState"
         )
 
         central_node_low.move_to_on()
@@ -188,113 +179,171 @@ class TestConfigureCommandNotAllowedPropagation:
             DevState.ON,
         )
 
+        assign_input_str = prepare_json_args_for_centralnode_commands(
+            "assign_resources_low", command_input_factory
+        )
+        central_node_low.store_resources(assign_input_str)
+
+        assert_that(event_tracer).described_as(
+            "FAILED ASSUMPTION AFTER ASSIGNRESOURCES COMMAND: "
+            "Subarray Node device"
+            f"({subarray_node_low.subarray_node.dev_name()}) "
+            "is expected to be in IDLE obstate",
+        ).within_timeout(TIMEOUT).has_change_event_occurred(
+            subarray_node_low.subarray_node,
+            "obsState",
+            ObsState.IDLE,
+        )
+
+        # Preparing input files
+        configure_input_str = prepare_json_args_for_commands(
+            "configure_low", command_input_factory
+        )
+        # Inducing Fault
+        #
         # Setting Defects on Devices
         sdp_subarray_sim.SetDefective(COMMAND_NOT_ALLOWED_DEFECT)
-        _, unique_id = central_node_low.perform_action(
-            "AssignResources", assign_input_json
-        )
-        log_events(
-            {central_node_low.central_node: ["longRunningCommandResult"]}
-        )
-        exception_message = (
-            "Exception occurred on the following devices: "
-            + f"{low_sdp_subarray_leaf_node}: ska_tmc_common.exceptions"
-            + ".CommandNotAllowed: Command is not allowed"
+
+        _, pytest.unique_id = subarray_node_low.execute_transition(
+            "Configure", configure_input_str
         )
         assert_that(event_tracer).described_as(
-            "FAILED ASSUMPTION ATER ASSIGN RESOURCES: "
-            "Central Node device"
-            f"({central_node_low.central_node.dev_name()}) "
+            'FAILED ASSUMPTION IN "WHEN" STEP: '
+            '"Configure command is invoked on a defective SDP Subarray"'
+            "Subarray Node device"
+            f"({subarray_node_low.subarray_node.dev_name()}) "
+            "is expected to be in FAULT obstate",
+        ).within_timeout(TIMEOUT).has_change_event_occurred(
+            subarray_node_low.subarray_node,
+            "obsState",
+            ObsState.FAULT,
+        )
+
+        exception_message = (
+            " The invocation of the Configure command failed on Sdp "
+            + "Subarray Device low-sdp/subarray/01"
+        )
+
+        log_events(
+            {subarray_node_low.subarray_node: ["longRunningCommandResult"]}
+        )
+
+        assert_that(event_tracer).described_as(
+            "FAILED ASSUMPTION AFTER CONFIGURE: "
+            f"({subarray_node_low.subarray_node.dev_name()}) "
             "is expected have longRunningCommandResult"
             "(ResultCode.FAILED,exception)",
         ).within_timeout(
             TIMEOUT
         ).has_desired_result_code_message_in_lrcr_event(
-            central_node_low.central_node,
+            subarray_node_low.subarray_node,
             [exception_message],
-            unique_id[0],
+            pytest.unique_id[0],
             ResultCode.FAILED,
         )
 
-    # @pytest.mark.SKA_low
-    # def test_assign_command_not_allowed_propagation_mccs_ln_low(
-    #     self,
-    #     central_node_low: CentralNodeWrapperLow,
-    #     event_tracer: TangoEventTracer,
-    #     simulator_factory: SimulatorFactory,
-    #     command_input_factory: JsonFactory,
-    # ):
-    #     """Verify command not allowed exception propagation
-    #     from MccsLeafNodes."""
-    #     mccs_subarray_sim = simulator_factory.get_or_create_simulator_device(
-    #         SimulatorDeviceType.LOW_SDP_DEVICE
-    #     )
-    #     mccs_master_sim = simulator_factory.get_or_create_simulator_device(
-    #         SimulatorDeviceType.MCCS_MASTER_DEVICE
-    #     )
-    #
-    #     # Event Subscriptions
-    #     event_tracer.subscribe_event(
-    #         central_node_low.central_node, "telescopeState"
-    #     )
-    #     event_tracer.subscribe_event(
-    #         central_node_low.subarray_node, "obsState"
-    #     )
-    #     event_tracer.subscribe_event(
-    #         central_node_low.central_node, "longRunningCommandResult"
-    #     )
-    #     event_tracer.subscribe_event(mccs_subarray_sim, "obsState")
-    #     # Preparing input arguments
-    #     assign_input_json = prepare_json_args_for_centralnode_commands(
-    #         "assign_resources_low", command_input_factory
-    #     )
-    #
-    #     log_events(
-    #         {
-    #             central_node_low.central_node: ["longRunningCommandResult"],
-    #             mccs_subarray_sim: ["obsState"],
-    #             central_node_low.subarray_node: ["obsState"],
-    #         }
-    #     )
-    #
-    #     central_node_low.move_to_on()
-    #     assert_that(event_tracer).described_as(
-    #         "FAILED ASSUMPTION AFTER ON COMMAND: "
-    #         "Central Node device"
-    #         f"({central_node_low.central_node.dev_name()}) "
-    #         "is expected to be in TelescopeState ON",
-    #     ).within_timeout(TIMEOUT).has_change_event_occurred(
-    #         central_node_low.central_node,
-    #         "telescopeState",
-    #         DevState.ON,
-    #     )
-    #
-    #     # Setting Defects on Devices
-    #     mccs_master_sim.SetDefective(COMMAND_NOT_ALLOWED_DEFECT)
-    #     # Execute Assign command and verify successful execution
-    #     _, unique_id = central_node_low.perform_action(
-    #         "AssignResources", assign_input_json
-    #     )
-    #     # Constructing the error message
-    #     exception_message = (
-    #         f"{mccs_master_leaf_node}:
-    #         The invocation of the Allocate command"
-    #         + f" is failed on MCCS Controller device {mccs_controller}"
-    #     )
-    #
-    #     exception_message2 = "ska_tmc_common.exceptions.CommandNotAllowed"
-    #
-    #     assert_that(event_tracer).described_as(
-    #         "FAILED ASSUMPTION AFTER ASSIGN RESOURCES: "
-    #         "Central Node device"
-    #         f"({central_node_low.central_node.dev_name()}) "
-    #         "is expected have longRunningCommandResult"
-    #         "(ResultCode.FAILED,exception)",
-    #     ).within_timeout(
-    #         TIMEOUT
-    #     ).has_desired_result_code_message_in_lrcr_event(
-    #         central_node_low.central_node,
-    #         [exception_message, exception_message2],
-    #         unique_id[0],
-    #         ResultCode.FAILED,
-    #     )
+    @pytest.mark.SKA_low16
+    def test_configure_command_not_allowed_propagation_mccs_ln_low(
+        self,
+        central_node_low: CentralNodeWrapperLow,
+        subarray_node_low: SubarrayNodeWrapperLow,
+        event_tracer: TangoEventTracer,
+        simulator_factory: SimulatorFactory,
+        command_input_factory: JsonFactory,
+    ):
+        """Verify command not allowed exception propagation from MCCSLeafNodes
+        ."""
+
+        mccs_subarray_sim = simulator_factory.get_or_create_simulator_device(
+            SimulatorDeviceType.MCCS_SUBARRAY_DEVICE
+        )
+
+        # Event Subscriptions
+        event_tracer.subscribe_event(
+            central_node_low.central_node, "telescopeState"
+        )
+        event_tracer.subscribe_event(
+            central_node_low.central_node, "longRunningCommandResult"
+        )
+
+        event_tracer.subscribe_event(
+            subarray_node_low.subarray_node, "longRunningCommandResult"
+        )
+        event_tracer.subscribe_event(
+            subarray_node_low.subarray_node, "obsState"
+        )
+
+        central_node_low.move_to_on()
+        assert_that(event_tracer).described_as(
+            "FAILED ASSUMPTION AFTER ON COMMAND: "
+            "Central Node device"
+            f"({central_node_low.central_node.dev_name()}) "
+            "is expected to be in TelescopeState ON",
+        ).within_timeout(TIMEOUT).has_change_event_occurred(
+            central_node_low.central_node,
+            "telescopeState",
+            DevState.ON,
+        )
+
+        assign_input_str = prepare_json_args_for_centralnode_commands(
+            "assign_resources_low", command_input_factory
+        )
+        central_node_low.store_resources(assign_input_str)
+
+        assert_that(event_tracer).described_as(
+            "FAILED ASSUMPTION AFTER ASSIGNRESOURCES COMMAND: "
+            "Subarray Node device"
+            f"({subarray_node_low.subarray_node.dev_name()}) "
+            "is expected to be in IDLE obstate",
+        ).within_timeout(TIMEOUT).has_change_event_occurred(
+            subarray_node_low.subarray_node,
+            "obsState",
+            ObsState.IDLE,
+        )
+
+        # Preparing input files
+        configure_input_str = prepare_json_args_for_commands(
+            "configure_low", command_input_factory
+        )
+        # Inducing Fault
+        #
+        # Setting Defects on Devices
+        mccs_subarray_sim.SetDefective(COMMAND_NOT_ALLOWED_DEFECT)
+
+        _, pytest.unique_id = subarray_node_low.execute_transition(
+            "Configure", configure_input_str
+        )
+        assert_that(event_tracer).described_as(
+            'FAILED ASSUMPTION IN "WHEN" STEP: '
+            '"Configure command is invoked on a defective MCCS Subarray"'
+            "Subarray Node device"
+            f"({subarray_node_low.subarray_node.dev_name()}) "
+            "is expected to be in FAULT obstate",
+        ).within_timeout(TIMEOUT).has_change_event_occurred(
+            subarray_node_low.subarray_node,
+            "obsState",
+            ObsState.FAULT,
+        )
+
+        exception_message = (
+            " The invocation of the Configure command failed on Mccs "
+            + "Subarray Device low-mccs/subarray/01"
+        )
+
+        log_events(
+            {subarray_node_low.subarray_node: ["longRunningCommandResult"]}
+        )
+
+        assert_that(event_tracer).described_as(
+            "FAILED ASSUMPTION AFTER CONFIGURE: "
+            f"({subarray_node_low.subarray_node.dev_name()}) "
+            "is expected have longRunningCommandResult"
+            "(ResultCode.FAILED,exception)",
+        ).within_timeout(
+            TIMEOUT
+        ).has_desired_result_code_message_in_lrcr_event(
+            subarray_node_low.subarray_node,
+            [exception_message],
+            pytest.unique_id[0],
+            ResultCode.FAILED,
+        )
