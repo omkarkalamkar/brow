6bd3799cad4d31959cac4353cd220d8a9ca02612
ganesh.shinde <ganeshshinde.ska@gmail.com>
2025-07-01 17:49:08 +0530

    SAH-1735: minor fix

--- tests/tmc/test_error_propogation_xtp_73766.py
+++ tests/tmc/test_error_propogation_xtp_73766.py
@@ -248,8 +248,6 @@ def validate_error_message_reporting(
     pytest.defective_subarray.SetDefective(json.dumps({"enabled": False}))
     pytest.defective_subarray.ResetDelayInfo()
 
-    event_tracer.clear_events()
-
 
 @then("the TMC SubarrayNode transitions to FAULT obsState")
 def validate_subarry_obsState(
