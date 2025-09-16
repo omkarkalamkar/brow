4f345b5f365abfe4534570f8501195247f8545e2
abhijeet.deshkar.ska <abhijeet.deshkar.ska@gmail.com>
2025-09-16 23:09:04 +0530

    SAH-1783: Unskip test

--- tests/tmc/tmc_new_iTH/tests/test_restart_command_xtp_82856.py
+++ tests/tmc/tmc_new_iTH/tests/test_restart_command_xtp_82856.py
@@ -108,7 +108,7 @@ def _check_abort_flow(
         )
 
 
-@pytest.mark.skip
+@pytest.mark.SKA_tmc_low_restart
 @scenario(
     "../tmc/tmc_new_iTH/features/xtp_82856.feature",
     "Test Restart Command when TMC subarray transitions to "
