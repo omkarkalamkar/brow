c7efe54203a658b710ece7b17b8df913b78e8df1
truptin.ska <truptin.ska@gmail.com>
2025-10-14 18:25:30 +0530

    sah-1799:execute all tests

--- tests/tmc/tmc_new_iTH/tests/test_restart_command_xtp_82856.py
+++ tests/tmc/tmc_new_iTH/tests/test_restart_command_xtp_82856.py
@@ -108,7 +108,7 @@ def _check_abort_flow(
         )
 
 
-@pytest.mark.SKA_tmc_low_restart12
+@pytest.mark.SKA_tmc_low_restart
 @scenario(
     "../tmc/tmc_new_iTH/features/xtp_82856.feature",
     "Test Restart Command when TMC subarray transitions to "
