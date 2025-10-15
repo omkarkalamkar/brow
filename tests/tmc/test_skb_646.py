64921c333d5899b2338fa9b4b21535268f8c58e6
apurvapatkar <apurva.ska@gmail.com>
2025-10-15 13:06:44 +0530

    SAH-1806: Execute test SKB-646.

--- tests/tmc/test_skb_646.py
+++ tests/tmc/test_skb_646.py
@@ -32,7 +32,7 @@ from tests.resources.test_support.constant_low import (
 
 
 # @pytest.mark.skip(reason="SKB-908 to be resolved under sah-1798")
-@pytest.mark.SKA_low_skb646
+@pytest.mark.SKA_low
 @scenario(
     "../features/tmc/SKB_646.feature",
     "Verify SKB-646",
