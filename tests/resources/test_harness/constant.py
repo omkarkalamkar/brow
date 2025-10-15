54376f07582935a60af674376c131bf45755e8aa
yogeshk.ska <yogeshk.ska@gmail.com>
2025-10-15 21:10:55 +0530

    HM-766: command not allowed test case fix

--- tests/resources/test_harness/constant.py
+++ tests/resources/test_harness/constant.py
@@ -156,7 +156,7 @@ COMMAND_FAILED_WITH_EXCEPTION_OBSSTATE_IDLE = {
 COMMAND_NOT_ALLOWED_DEFECT = json.dumps(
     {
         "enabled": True,
-        "fault_type": FaultType.COMMAND_NOT_ALLOWED,
+        "fault_type": FaultType.COMMAND_NOT_ALLOWED_BEFORE_QUEUING,
         "error_message": "Command is not allowed",
         "result": ResultCode.FAILED,
     }
