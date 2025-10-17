43416d57de68c01b9db858b9cf108c4b19d70ea0
ganesh.shinde <ganeshshinde.ska@gmail.com>
2025-10-17 13:52:56 +0530

    SAH-1798: all test run

--- tests/tmc/test_skb_908_assign_flow.py
+++ tests/tmc/test_skb_908_assign_flow.py
@@ -25,7 +25,7 @@ from tests.resources.test_support.common_utils.tmc_helpers import (
 from tests.resources.test_support.constant_low import TIMEOUT
 
 
-@pytest.mark.repeat(10)
+@pytest.mark.repeat(5)
 @pytest.mark.skb908
 @pytest.mark.SKA_low
 @scenario(
