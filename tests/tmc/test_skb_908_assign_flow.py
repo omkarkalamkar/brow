8bfcf780df4c1cb8c5670ea06a64d24493f4170f
ganesh.shinde <ganeshshinde.ska@gmail.com>
2025-10-17 13:35:41 +0530

    SAH-1798: mccs dev tag

--- tests/tmc/test_skb_908_assign_flow.py
+++ tests/tmc/test_skb_908_assign_flow.py
@@ -25,6 +25,8 @@ from tests.resources.test_support.common_utils.tmc_helpers import (
 from tests.resources.test_support.constant_low import TIMEOUT
 
 
+@pytest.mark.repeat(10)
+@pytest.mark.skb908
 @pytest.mark.SKA_low
 @scenario(
     "../features/tmc/skb_908.feature",
