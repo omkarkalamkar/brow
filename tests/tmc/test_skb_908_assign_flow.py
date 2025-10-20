4e6e675554c709887caf0ef55f5eaeea4de2fcf0
yogeshk.ska <yogeshk.ska@gmail.com>
2025-10-20 18:08:23 +0530

    HM-766: auto recovery test case update

--- tests/tmc/test_skb_908_assign_flow.py
+++ tests/tmc/test_skb_908_assign_flow.py
@@ -25,7 +25,6 @@ from tests.resources.test_support.common_utils.tmc_helpers import (
 from tests.resources.test_support.constant_low import TIMEOUT
 
 
-@pytest.mark.skip
 @pytest.mark.SKA_low
 @scenario(
     "../features/tmc/skb_908.feature",
