6ac4bc55df4fa9f413e767ff18f41e49d0554338
Akshay Sonawane <akshayss.ska@gmail.com>
2025-10-23 14:41:37 +0530

    HM-773: fix the test case

--- tests/tmc/tmc_new_iTH/tests/test_assign_auto_recovery.py
+++ tests/tmc/tmc_new_iTH/tests/test_assign_auto_recovery.py
@@ -260,20 +260,13 @@ def verify_tmc_subarray_lrcr_failed(
     failed_devices: str,
 ):
     """Verifies that tmc subarray lrcr failed."""
-    if pytest.is_successive_configure:
-        failed_message = (
-            "Exception occurred on the following devices: "
-            "low-tmc/subarray-leaf-node-csp/01: Exception occurred "
-            "on device: low-csp/subarray/01 and Recovery Successful, "
-            "Subarray transitioned back to READY with previous configuration."
-        )
-    else:
-        failed_message = (
-            "Exception occurred on the following devices: "
-            "low-tmc/subarray-leaf-node-sdp/01: Device defective. "
-            "and Recovery Successful, "
-            "Subarray transitioned back to IDLE"
-        )
+
+    failed_message = (
+        "Exception occurred on the following devices: "
+        "low-tmc/subarray-leaf-node-sdp/01: Device defective. "
+        "and Recovery Successful, "
+        "Subarray transitioned back to EMPTY"
+    )
     assert_that(event_tracer).described_as(
         "TMC Subarray Leaf Node "
         f"({tmc.subarray_node}) "
