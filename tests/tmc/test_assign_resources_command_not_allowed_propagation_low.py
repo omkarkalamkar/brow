a7bdafb9356bacd8918b68bf08277eba2dbcf3e4
Jatin Sharma <jatins.ska@gmail.com>
2025-11-13 08:10:57 +0530

    HM-795 : Resolving minor issues in test cases

--- tests/tmc/test_assign_resources_command_not_allowed_propagation_low.py
+++ tests/tmc/test_assign_resources_command_not_allowed_propagation_low.py
@@ -261,7 +261,9 @@ class TestAssignCommandNotAllowedPropagation:
             + f" is failed on MCCS Controller device {mccs_controller}"
         )
 
-        exception_message2 = "ska_tmc_common.exceptions.CommandNotAllowed"
+        exception_message2 = (
+            "ska_tmc_simulators.dependecies.exceptions.CommandNotAllowed"
+        )
 
         assert_that(event_tracer).described_as(
             "FAILED ASSUMPTION AFTER ASSIGN RESOURCES: "
