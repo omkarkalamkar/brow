c805f4e32b3d2048263a9190253b8cc95d773e43
akshaysonawane <akshayss.ska@gmail.com>
2025-12-22 16:44:22 +0530

    HM-810: update the mark

--- tests/tmc/tmc_new_iTH/tests/test_assign_auto_recovery.py
+++ tests/tmc/tmc_new_iTH/tests/test_assign_auto_recovery.py
@@ -34,7 +34,6 @@ FAILED_DEVICE_MAP = {
 }
 
 
-@pytest.mark.aki
 @pytest.mark.SKA_low
 @scenario(
     "../tmc/tmc_new_iTH/features/assignresources_auto_recovery.feature",
@@ -44,7 +43,6 @@ def test_assign_auto_recovery():
     """BDD test scenario to verify auto recovery when assignresources failed"""
 
 
-@pytest.mark.aki
 @pytest.mark.SKA_low
 @scenario(
     "../tmc/tmc_new_iTH/features/assignresources_auto_recovery.feature",
@@ -56,7 +54,6 @@ def test_auto_recovery_failed():
     """
 
 
-@pytest.mark.aki
 @pytest.mark.SKA_low
 @scenario(
     "../tmc/tmc_new_iTH/features/assignresources_auto_recovery.feature",
