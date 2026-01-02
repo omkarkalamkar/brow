ab52fc09142afe6023b2a716fb9fd7255e5c642f
apurvapatkar <apurva.ska@gmail.com>
2026-01-02 20:15:29 +0530

    SAH-1844: Execute failing tests.

--- tests/tmc/test_xtp_81339_timeout_abort.py
+++ tests/tmc/test_xtp_81339_timeout_abort.py
@@ -47,7 +47,7 @@ exception_messages = {
     "MCCS": (
         '[3, "Exception occurred on the following devices: '
         + "low-tmc/subarray-leaf-node-mccs/01: Timeout has occurred, "
-        + 'command failed"]',
+        + 'command failed"]'
     ),
 }
 
