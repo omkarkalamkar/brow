1e787423a8f69d1acaa38ddfb88895f5e2e1b007
apurvapatkar <apurva.ska@gmail.com>
2026-03-18 14:08:35 +0530

    SAH-1883: Execute observation with 4 subarrays..

--- tests/resources/test_harness/central_node_low.py
+++ tests/resources/test_harness/central_node_low.py
@@ -433,6 +433,7 @@ class CentralNodeWrapperLow(object):
             self.subarray_devices.get("sdp_subarray"),
             self.subarray_devices.get("mccs_subarray"),
         ]
+        LOGGER.info("device_to_on_list: %s", device_to_on_list)
         for device in device_to_on_list:
             device.SetDirectState(subarray_state)
 
