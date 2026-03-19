d806edf3d35f4f1f039d5ebb7a1dc5cfee4396ff
truptin.ska <truptin.ska@gmail.com>
2026-03-19 17:16:13 +0530

    sah-1884:Receive Address Enabled

--- tests/tmc/test_tmc_max_subarry.py
+++ tests/tmc/test_tmc_max_subarry.py
@@ -24,7 +24,10 @@ from tests.resources.test_harness.constant import (
 from tests.resources.test_harness.subarray_node_low import (
     SubarrayNodeWrapperLow,
 )
-from tests.resources.test_harness.utils.common_utils import JsonFactory
+from tests.resources.test_harness.utils.common_utils import (
+    JsonFactory,
+    get_centralnode_input_json,
+)
 from tests.resources.test_support.common_utils.result_code import ResultCode
 from tests.resources.test_support.common_utils.tmc_helpers import (
     prepare_json_args_for_centralnode_commands,
@@ -135,12 +138,12 @@ def given_subarray_in_ready(
 
     # Set sdp_subarray_proxy and assign rreceive address
 
-    # receive_address = get_centralnode_input_json(
-    #     "ReceiveAddresses_with_68_stations"
-    # )
-    # central_node_low.subarray_devices[
-    #     "sdp_subarray"
-    # ].SetDirectreceiveAddresses(receive_address)
+    receive_address = get_centralnode_input_json(
+        "ReceiveAddresses_with_68_stations"
+    )
+    central_node_low.subarray_devices[
+        "sdp_subarray"
+    ].SetDirectreceiveAddresses(receive_address)
 
     configure_input_json = prepare_json_args_for_commands(
         "configure_8beams_68_stations", command_input_factory
