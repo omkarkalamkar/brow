641d81f52bc20652200b73f6929748c3c6fab5c6
apurvapatkar <apurva.ska@gmail.com>
2026-03-23 20:03:29 +0530

    SAH-1883: Execute tests.

--- tests/tmc/test_healthinfo.py
+++ tests/tmc/test_healthinfo.py
@@ -13,6 +13,7 @@ from tests.resources.test_harness.helpers import LOGGER, get_device_simulators
 state = {}
 
 
+@pytest.mark.xfail(reason="Refactor test to use event tracer")
 @pytest.mark.SKA_low
 @scenario(
     "../features/tmc/xtp_102560_check_healthinfo.feature",
