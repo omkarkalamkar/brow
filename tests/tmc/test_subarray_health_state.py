9c9c3d16e57d3778129ffb84fed9331208f37800
apurvapatkar <apurva.ska@gmail.com>
2026-03-23 17:37:57 +0530

    SAH-1883: Execute tests.

--- tests/tmc/test_subarray_health_state.py
+++ tests/tmc/test_subarray_health_state.py
@@ -11,7 +11,7 @@ from tests.resources.test_harness.helpers import get_device_simulators
 state = {}
 
 
-@pytest.xfail(reason="Refactor test to use event tracer")
+@pytest.mark.xfail(reason="Refactor test to use event tracer")
 @pytest.mark.SKA_low
 @scenario(
     "../features/tmc/check_subarray_healthstate.feature",
