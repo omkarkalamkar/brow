640028154debcc5e90195a9b4f1d08d17bcc8082
devesh.ska <devesh.ska@gmail.com>
2026-03-31 17:19:44 +0530

    HM-872: update test

--- tests/tmc/test_healthinfo.py
+++ tests/tmc/test_healthinfo.py
@@ -14,7 +14,6 @@ from tests.resources.test_harness.helpers import LOGGER, get_device_simulators
 state = {}
 
 
-@pytest.mark.skip(reason="frequent failure")
 @pytest.mark.SKA_low
 @scenario(
     "../features/tmc/xtp_102560_check_healthinfo.feature",
