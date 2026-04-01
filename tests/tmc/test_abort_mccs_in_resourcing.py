8d8b63658ee10ea22f50fbc52f8c500fa0ed5343
abhijeet.deshkar.ska <abhijeet.deshkar.ska@gmail.com>
2026-04-01 18:38:43 +0530

    SAH-1898: Run marked tests

--- tests/tmc/test_abort_mccs_in_resourcing.py
+++ tests/tmc/test_abort_mccs_in_resourcing.py
@@ -29,6 +29,7 @@ from tests.resources.test_support.common_utils.tmc_helpers import (
 from tests.resources.test_support.constant_low import TIMEOUT
 
 
+@pytest.mark.intl
 @pytest.mark.SKA_low
 @scenario(
     "../features/tmc/check_mccs_abort.feature",
