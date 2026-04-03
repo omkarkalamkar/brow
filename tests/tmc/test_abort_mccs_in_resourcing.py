e2e55f39da7db961c70049c0d3a008fb7f75b7f9
abhijeet.deshkar.ska <abhijeet.deshkar.ska@gmail.com>
2026-04-03 16:47:41 +0530

    SAH-1898: Remove markers

--- tests/tmc/test_abort_mccs_in_resourcing.py
+++ tests/tmc/test_abort_mccs_in_resourcing.py
@@ -29,7 +29,6 @@ from tests.resources.test_support.common_utils.tmc_helpers import (
 from tests.resources.test_support.constant_low import TIMEOUT
 
 
-@pytest.mark.intl
 @pytest.mark.SKA_low
 @scenario(
     "../features/tmc/check_mccs_abort.feature",
