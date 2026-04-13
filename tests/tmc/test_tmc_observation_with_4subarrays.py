ee56b83eb9d89cf9c229afb714f079eb72468c89
ganesh.shinde <ganeshshinde.ska@gmail.com>
2026-04-13 16:51:30 +0530

    SAH-1903: commented out some logs

--- tests/tmc/test_tmc_observation_with_4subarrays.py
+++ tests/tmc/test_tmc_observation_with_4subarrays.py
@@ -30,7 +30,6 @@ configure_logging(logging.DEBUG)
 LOGGER = logging.getLogger(__name__)
 
 
-# @pytest.mark.SKA_tmc_low_multiple_subarrays
 @pytest.mark.SKA_tmc_low_four_subarrays
 @scenario(
     "../features/tmc/tmc_observation_with_four_subarray.feature",
