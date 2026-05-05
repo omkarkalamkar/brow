c3f68f563f24c4e0185398e782a73e15c4c8ce60
yogeshk.ska <yogeshk.ska@gmail.com>
2026-05-05 13:05:51 +0530

    HM-897: test case fix

--- tests/tmc/tmc_new_iTH/tests/test_assign_auto_recovery.py
+++ tests/tmc/tmc_new_iTH/tests/test_assign_auto_recovery.py
@@ -395,19 +395,21 @@ def verify_tmc_subarray_lrcr_failed(
             )
         elif failed_device == "CSP":
             failed_message = (
-                "low-tmc/subarray/01: Exception occurred on the following "
-                "devices: "
-                "low-tmc/subarray-leaf-node-csp/01: Exception occurred on "
-                "device: low-csp/subarray/01 and Recovery "
-                "Successful, Subarray transitioned back to EMPTY"
+                "low-tmc/subarray/01: "
+                "Exception occurred on the following devices: "
+                "low-tmc/subarray-leaf-node-csp/01: "
+                "Exception occurred on devices: low-csp/subarray/01: "
+                "Exception occurred on device: low-csp/subarray/01 "
+                "and Recovery Successful, Subarray transitioned back to EMPTY"
             )
         elif failed_device == "MCCS":
             failed_message = (
-                "low-tmc/subarray/01: Exception occurred on the following "
-                "devices: "
-                "low-tmc/subarray-leaf-node-mccs/01: Exception occurred on "
-                "device: low-mccs/subarray/01 and Recovery "
-                "Successful, Subarray transitioned back to EMPTY"
+                "low-tmc/subarray/01: "
+                "Exception occurred on the following devices: "
+                "low-tmc/subarray-leaf-node-mccs/01: "
+                "Exception occurred on devices: low-mccs/subarray/01: "
+                "Exception occurred on device: low-mccs/subarray/01 "
+                "and Recovery Successful, Subarray transitioned back to EMPTY"
             )
     assert_that(event_tracer).described_as(
         "TMC Subarray Leaf Node "
