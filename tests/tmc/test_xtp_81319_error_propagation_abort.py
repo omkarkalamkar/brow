d51d0d1dc478e05a848cac8f9eeade6ef318549d
yogeshk.ska <yogeshk.ska@gmail.com>
2026-05-11 16:07:07 +0530

    HM-897: test case fix

--- tests/tmc/test_xtp_81319_error_propagation_abort.py
+++ tests/tmc/test_xtp_81319_error_propagation_abort.py
@@ -58,7 +58,8 @@ exception_messages = {
     "MCCS": (
         '[3, "Exception occurred on the following devices: '
         "low-tmc/subarray-leaf-node-mccs/01: "
-        "Exception occurred on devices: low-mccs/subarray/01: "
+        "Exception occurred on devices: "
+        "low-mccs/control/control: "
         'Exception occurred, command failed."]'
     ),
 }
